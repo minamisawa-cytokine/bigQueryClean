5172e3d1d21efadf4fa534aaa6dc24146539cd15
minamisawa <minamisawa@cytokine.jp>
2025-08-21 17:51:05 +0900

    プロジェクトIDは指定必要/パーティション情報無し

--- collect_table_info.sql
+++ collect_table_info.sql
@@ -1,6 +1,7 @@
 -- BigQuery テーブル使用状況データ収集・挿入クエリ
 -- 現在のプロジェクトの全データセット・テーブル情報を収集
 
+
 INSERT INTO `cleaning.table_inventory` (
   dataset_name,
   table_name,
@@ -23,25 +24,8 @@ WITH table_info AS (
     TIMESTAMP_MILLIS(last_modified_time) as last_modified_time,
     row_count,
     size_bytes
-  FROM `region-us.INFORMATION_SCHEMA.TABLES`
+  FROM `searchconsole-293711.INFORMATION_SCHEMA.TABLES`  -- PROJECT_ID を変更する場合はここも変更
   WHERE table_schema != 'INFORMATION_SCHEMA'
-),
-partition_info AS (
-  -- パーティション情報を取得
-  SELECT 
-    table_schema as dataset_name,
-    table_name,
-    partition_id,
-    CASE 
-      WHEN partition_id LIKE '%$%' THEN 'TIME_PARTITIONING'
-      WHEN partition_id != '__NULL__' THEN 'RANGE_PARTITIONING'
-      ELSE NULL
-    END as partition_type,
-    -- パーティションカラム名は直接取得できないため、別途調査が必要
-    CAST(NULL AS STRING) as partition_column
-  FROM `region-us.INFORMATION_SCHEMA.PARTITIONS`
-  WHERE partition_id != '__UNPARTITIONED__'
-  GROUP BY 1, 2, 3, 4
 ),
 last_query_info AS (
   -- 最終クエリ実行日時を取得（APIクエリのみ、プレビュー除外）
@@ -49,7 +33,7 @@ last_query_info AS (
     referenced_tables.dataset_id as dataset_name,
     referenced_tables.table_id as table_name,
     MAX(creation_time) as last_query_time
-  FROM `region-us.INFORMATION_SCHEMA.JOBS_BY_PROJECT`,
+  FROM `searchconsole-293711.INFORMATION_SCHEMA.JOBS_BY_PROJECT`,  -- PROJECT_ID を変更する場合はここも変更
   UNNEST(referenced_tables) as referenced_tables
   WHERE 
     job_type = 'QUERY'
@@ -71,12 +55,9 @@ SELECT
   lqi.last_query_time,
   ti.row_count,
   ti.size_bytes,
-  pi.partition_type,
-  pi.partition_column
+  CAST(NULL AS STRING) as partition_type,
+  CAST(NULL AS STRING) as partition_column
 FROM table_info ti
-LEFT JOIN partition_info pi 
-  ON ti.dataset_name = pi.dataset_name 
-  AND ti.table_name = pi.table_name
 LEFT JOIN last_query_info lqi 
   ON ti.dataset_name = lqi.dataset_name 
   AND ti.table_name = lqi.table_name
